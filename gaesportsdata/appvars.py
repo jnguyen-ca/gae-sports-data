61d7ae5c220a7a21c2ce358ce5f4385a908841d9
John <nguyen3@ualberta.ca>
2017-05-18 23:59:40 -0600

    add ability to edit strings and lists app vars #6

--- gaesportsdata/appvars.py
+++ gaesportsdata/appvars.py
@@ -5,10 +5,7 @@
 from google.appengine.api import users
 import re
 import ast
-import json
-import cgi
 import logging
-import pprint
 
 import flask
 
@@ -21,63 +18,65 @@ def handle_request():
         flask.abort(403)
     
     app_var_id = flask.request.form['app-var-id']
-    logging.info(app_var_id)
     if app_var_id not in constants.APPVAR_DISPLAY_LIST:
         flask.abort(400)
     
     request_type = flask.request.form['request-type']
     request_value = flask.request.form['request-value']
-    logging.info('Request : (%s), AppVar : (%s)' % (request_type, app_var_id))
+    logging.debug('Request : (%s), AppVar : (%s)' % (request_type, app_var_id))
     
+    #TODO: other request types (e.g. delete entry)
     if request_type == 'add-edit-entry':
-        app_var = models.ApplicationVariable.get_app_var(app_var_id)
-    else:
-        flask.abort(400)
-    
-    try:
-        # leave numeric strings alone but evaluate lists and dicts
-        if not request_value.isnumeric():
-            request_value = ast.literal_eval(request_value)
+        try:
+            # leave numeric strings alone but evaluate lists and dicts
+            if not request_value.isnumeric():
+                request_value = ast.literal_eval(request_value)
+                
+                # convert everything to unicode
+                if isinstance(request_value, str):
+                    request_value = request_value.decode('utf-8')
+                elif isinstance(request_value, list):
+                    request_value = [x.decode('utf-8') if isinstance(x, str) else x for x in request_value]
+                elif isinstance(request_value, dict):
+                    request_value = {k.decode('utf8'): v.decode('utf8') for k, v in request_value.items()}
+        except (SyntaxError, ValueError):
+            flask.response = flask.jsonify({'message' : 'Incorrect formatting provided. Please provide valid Python literal structure.'})
+            flask.response.status_code(400)
+            return flask.response
+        except AttributeError:
+            flask.response = flask.jsonify({'message' : 'Currently nested dicts are not supported. Please do 1 level at a time.'})
+            flask.response.status_code=400
+            return flask.response
+        
+        try:
+            key_list = re.findall('\[([^\]]+)\]', flask.request.form['dict-ancestor-keys'])
+            key_list = [key.strip() for key in key_list]
+            entry_key = flask.request.form['dict-entry-key']
+            entry_level = flask.request.form['dict-entry-level']
+        except KeyError:
+            entry_key = None
+        
+        if key_list or entry_key:
+            if entry_key:
+                key_list.append(entry_key)
+                
+            logging.debug('Setting dict entry: %s' % (str(key_list)))
+            app_var = models.ApplicationVariable.get_app_var(app_var_id)
+            set_in_dict(app_var, key_list, request_value)
             
-            # convert everything to unicode
-            if isinstance(request_value, str):
-                request_value = request_value.decode('utf-8')
-            elif isinstance(request_value, list):
-                request_value = [x.decode('utf-8') if isinstance(x, str) else x for x in request_value]
-            elif isinstance(request_value, dict):
-                request_value = {k.decode('utf8'): v.decode('utf8') for k, v in request_value.items()}
-    except (SyntaxError, ValueError):
-        flask.response = flask.jsonify({'message' : 'Incorrect formatting provided. Please provide valid Python literal structure.'})
-        flask.response.status_code(400)
-        return flask.response
-    except AttributeError:
-        flask.response = flask.jsonify({'message' : 'Currently nested dicts are not supported. Please do 1 level at a time.'})
-        flask.response.status_code=400
-        return flask.response
-    
-    try:
-        key_list = re.findall('\[([^\]]+)\]', flask.request.form['dict-ancestor-keys'])
-        key_list = [key.strip() for key in key_list]
-        entry_key = flask.request.form['dict-entry-key']
-        entry_level = flask.request.form['dict-entry-level']
-    except KeyError:
-        entry_key = None
-        
-    if entry_key:
-        key_list.append(entry_key)
-        logging.info('Setting dict entry: %s' % (str(key_list)))
-        set_in_dict(app_var, key_list, request_value)
-        
-        models.ApplicationVariable.set_app_var(app_var_id, app_var)
-        
-        display_dict_entry = flask.get_template_attribute('macros.html', 'display_dict_entry')
-        return display_dict_entry(entry_key, request_value, int(entry_level))
+            models.ApplicationVariable.set_app_var(app_var_id, app_var)
+            
+            if entry_key:
+                display_dict_entry = flask.get_template_attribute('macros.html', 'display_dict_entry')
+                return display_dict_entry(entry_key, request_value, int(entry_level))
+            else:
+                display = flask.get_template_attribute('macros.html', 'display')
+                return display(request_value)
+        #TODO: allow creation of new appvars from scratch
+#         else:
+#             models.ApplicationVariable.set_app_var(app_var_id, request_value)
     else:
-        # TODO: haven't finished yet
         flask.abort(400)
-        
-    display = flask.get_template_attribute('macros.html', 'display')
-    return display(request_value)
 
 def get_from_dict(dictionary, key_list):
     """Get a dictionary entry via a list of keys
--- gaesportsdata/static/js/appvars.js
+++ gaesportsdata/static/js/appvars.js
@@ -3,6 +3,11 @@ var input_name_app_var_id = 'app-var-id'
 var input_name_request_type = 'request-type'
 var input_name_request_value = 'request-value'
 	
+// dict entry info
+var input_name_dict_ancestor_keys = 'dict-ancestor-keys'
+var input_name_dict_entry_key = 'dict-entry-key'
+var input_name_dict_entry_level = 'dict-entry-level'
+
 var request_type_add_edit_entry = 'add-edit-entry'
 
 function get_app_id($element) {
@@ -23,117 +28,163 @@ function get_ancestor_keys($entry) {
 }
 
 function add_dict_controls($element, is_admin=false) {
-	// should be what server expecting
-	var input_name_dict_ancestor_keys = 'dict-ancestor-keys'
-	var input_name_dict_entry_key = 'dict-entry-key'
-	var input_name_dict_entry_level = 'dict-entry-level'
-	
 	// add control scripts for all nested dicts (important for ajax response)
 	var $dict_element = $element.find('.dict-entries')
-	
 	var $dict_controls = $('<div></div>').addClass('dict-controls').prependTo($dict_element)
 	
 	if (is_admin) {
-		// only admin can ajax
-		function reset_form($form) {
-			$form.children(`[name="${input_name_dict_entry_key}"], 
-						[name="${input_name_request_value}"],
-						[type="submit"]`
-				)
-				.remove()
-		}
-		
-		$('<button>Add Entry</button>').prop('type', 'button')
-									.addClass('add-dict-entry')
-									.on('click', function() {
-										var $dict_form = $(this).siblings('form')
-										
-										if ($dict_form.children('[type="submit"]').length) {
-											// toggle
-											reset_form($dict_form)
-											return false;
-										}
-										
-										$dict_form.find('[name="'+input_name_request_type+'"]')
-													.prop('value', request_type_add_edit_entry)
-										
-										$('<input>').prop('name', input_name_dict_entry_key)
-													.prop('type','text')
-													.appendTo($dict_form)
-													
-										$('<input>').prop('name', input_name_request_value)
-													.prop('type', 'text')
-													.appendTo($dict_form)
-													
-										$('<button>Submit</button>').prop('type', 'submit')
-																	.appendTo($dict_form)
-									})
-									.appendTo($dict_controls)
-	
-		// base form
-		var $dict_form = $('<form></form>')
-		
-		$('<input>').prop('name', input_name_app_var_id)
-					.prop('type', 'hidden')
-					.prop('value', get_app_id($dict_element))
-					.appendTo($dict_form)
-					
-		$('<input>').prop('name', input_name_request_type)
-					.prop('type', 'hidden')
-					.appendTo($dict_form)
-					
-		$('<input>').prop('name', input_name_dict_ancestor_keys)
-					.prop('type', 'hidden')
-					.appendTo($dict_form)
-					
-		$('<input>').prop('name', input_name_dict_entry_level)
-					.prop('type', 'hidden')
-					.appendTo($dict_form)
-		// end base form
-					
-		$dict_form.on('submit', function() {
-			var $this = $(this)
-			$this.find('input').removeClass('error-highlight')
-			
-			// doing this on submit to ensure correct values
-			$this.find('[name="'+input_name_dict_ancestor_keys+'"]')
-					.prop('value', get_ancestor_keys($this))
-					
-			$this.find('[name="'+input_name_dict_entry_level+'"]')
-					.prop('value', $this.closest('.dict-entries')
-										.children('.dict-level')
-										.val()
-					)
-			
-			$.ajax({
-				url 	: window.location.pathname,
-				type	: 'POST',
-				data	: $(this).serialize(),
-				success	: function(data, text, jqXHR) {
-					reset_form($this)
-					
-					var $data = $(data)
-					
-					$this.closest('.dict-entries').children('.dict-entry:first').before($data)
-					add_dict_controls($data, is_admin)
-				},
-				error: function(jqXHR, textStatus, errorThrown) {
-					if (jqXHR.responseJSON) {
-						console.log(jqXHR.responseJSON.message)
-					}
-					$this.find('input').addClass('error-highlight')
-				}
+		$('<button>Add Entry</button>')
+			.prop('type', 'button')
+			.addClass('add-dict-entry')
+			.on('click', function() {
+				var $form = toggle_form($(this))
+				
+				$form.find('[name="'+input_name_request_type+'"]')
+							.prop('value', request_type_add_edit_entry)
+				
+				$form.find('[name="'+input_name_dict_entry_key+'"]')
+							.prop('type','text')
 			})
-			return false
-		});
-					
-		$dict_form.prependTo($dict_controls)
+			.appendTo($dict_controls)
+	
 	}
 	
-	$('<button>Minimize</button>').prop('type', 'button')
-									.addClass('minimize-dict-level')
-									.on('click', function() {
-										$(this).parent().nextAll().slideToggle();
-									})
-									.appendTo($dict_controls)
+	$('<button>Minimize</button>')
+		.prop('type', 'button')
+		.addClass('minimize-dict-level')
+		.on('click', function() {
+			$(this).parent().nextAll().slideToggle();
+		})
+		.appendTo($dict_controls)
+}
+
+function add_string_list_controls($element, is_admin=false) {
+	if (is_admin) {
+		var $elements = $element.find('.list-entries, :not(.list-entry) > .string-entry')
+		var $controls = $('<span></span>').addClass('controls').insertAfter($elements)
+		
+		$('<button>Edit</button>')
+			.prop('type', 'button')
+			.addClass('edit-entry')
+			.on('click', function() {
+				var $form = toggle_form($(this))
+				
+				var existing_value
+				var $entry = $(this).closest('.controls').prev()
+				
+				if ($entry.hasClass('string-entry')) {
+					existing_value = '"'+$entry.text()+'"'
+				}
+				else if ($entry.hasClass('list-entries')) {
+					existing_value = '['
+					$entry.find('.string-entry').each(function() {
+						existing_value += '"'+$(this).text()+'",'
+					})
+					existing_value = existing_value.substring(0, existing_value.length - 1)
+					existing_value += ']'
+				}
+				
+				$form.find('[name="'+input_name_request_value+'"]')
+							.prop('value', existing_value)
+				
+				$form.find('[name="'+input_name_request_type+'"]')
+							.prop('value', request_type_add_edit_entry)
+			})
+			.appendTo($controls)
+	}
+}
+
+function toggle_form($button) {
+	var $form = $button.siblings('form')
+	
+	if ($form.length) {
+		$form.toggle()
+	}
+	else {
+		$form = create_form()
+		$form.prependTo($button.parent())
+	}
+	
+	return $form
+}
+
+function create_form() {
+	var $form = $('<form></form>')
+	
+	// dict entry information
+	$('<input>').prop('name', input_name_dict_entry_key)
+				.prop('type','hidden')
+				.appendTo($form)
+	
+	$('<input>').prop('name', input_name_dict_ancestor_keys)
+				.prop('type', 'hidden')
+				.appendTo($form)
+				
+	$('<input>').prop('name', input_name_dict_entry_level)
+				.prop('type', 'hidden')
+				.appendTo($form)
+	
+	// basic information
+	$('<input>').prop('name', input_name_app_var_id)
+				.prop('type', 'hidden')
+				.appendTo($form)
+				
+	$('<input>').prop('name', input_name_request_type)
+				.prop('type', 'hidden')
+				.appendTo($form)
+				
+	$('<input>').prop('name', input_name_request_value)
+				.prop('type', 'text')
+				.appendTo($form)
+				
+	$('<button>Submit</button>').prop('type', 'submit')
+								.appendTo($form)
+				
+	$form.on('submit', function() {
+		var $this = $(this)
+		$this.find('input').removeClass('error-highlight')
+		
+		// doing this on submit to ensure correct values
+		$this.find('[name="'+input_name_app_var_id+'"]')
+				.prop('value', get_app_id($this))
+				
+		$this.find('[name="'+input_name_dict_ancestor_keys+'"]')
+				.prop('value', get_ancestor_keys($this))
+				
+		$this.find('[name="'+input_name_dict_entry_level+'"]')
+				.prop('value', $this.closest('.dict-entries')
+									.children('.dict-level')
+									.val()
+				)
+		
+		$.ajax({
+			url 	: window.location.pathname,
+			type	: 'POST',
+			data	: $(this).serialize(),
+			success	: function(data, text, jqXHR) {
+				$this.toggle()
+				
+				var $data = $(data)
+				
+				if ( $this.parent().hasClass('dict-controls') ) {
+					$this.closest('.dict-entries').children('.dict-entry:first').before($data)
+				}
+				else {
+					$this.closest('.controls').prev().replaceWith($data)
+				}
+				
+				add_dict_controls($data, true)
+				add_string_list_controls($data, true)
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				if (jqXHR.responseJSON) {
+					console.log(jqXHR.responseJSON.message)
+				}
+				$this.find('input').addClass('error-highlight')
+			}
+		})
+		return false
+	});
+	
+	return $form
 }
